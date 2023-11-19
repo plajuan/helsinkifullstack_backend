a0762f0702321a010adaa9ef41f3cb7f200447b0
Juan Pla <pla.juan@gmail.com>
2023-11-19 08:57:13 -0300

    3.9 phonebook backend step9

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
-
-app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.json())
+app.use(express.static('public'))
 
 let persons = [
     { 
