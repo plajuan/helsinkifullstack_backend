9cc3c7ba57e83e24e1b5ebd93da04a982917c2d1
Juan Pla <pla.juan@gmail.com>
2023-11-23 21:45:52 -0300

    3.12: Command-line database

--- mongo.js
+++ mongo.js
@@ -4,25 +4,40 @@ if (process.argv.length < 3) {
     console.log('Initialize the script with 3 arguments: password, name and number')
     process.exit(1)
 }
+
 const password = encodeURI(process.argv[2])
 const url = `mongodb+srv://plajuan:${password}@cluster0.7d32nss.mongodb.net/phoneApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
+const db = mongoose.connection
+db.once('open', () => { dbOperations() });
 
-const entrySchema = new mongoose.Schema({
-    name: String,
-    phoneNumber: String,
-})
-const Entry = mongoose.model('Entry', entrySchema)
+function dbOperations(){
+    const entrySchema = new mongoose.Schema({
+        name: String,
+        phoneNumber: String,
+    })
+    const Entry = mongoose.model('Entry', entrySchema)
+           
+    if (process.argv.length === 3){
+        console.log('phonebook:')
+        Entry.find({}).then(res => {
+            res.forEach(en => {
+              console.log(en.name, en.phoneNumber)
+            })
+            mongoose.connection.close()
+          })
+    } else {
+        const entry = new Entry({
+            name: process.argv[3],
+            phoneNumber: process.argv[4],
+        })
+        entry.save().then(res => {
+            console.log(`Added ${res.name} number ${res.phoneNumber} to phonebook`)
+            mongoose.connection.close()
+        })
+    }
+}
 
-const entry =  new Entry({
-    name: process.argv[3],
-    phoneNumber: process.argv[4],
-})
-
-entry.save().then(res => {
-    console.log(`Added ${res.name} number ${res.phoneNumber} to phonebook`)
-    mongoose.connection.close()
-})
-
+ 
